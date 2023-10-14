3379bdf8d5b24190624b987a6b16dadcee642728
CQF <635903897@qq.com>
2023-10-14 14:54:35 +0800

    参考eShopOnContainer抽象出共享层

---
+++ Infrastructure.Core/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Core
+{
+    public interface IRepository<TEntity> where TEntity : Entity, IAggregateRoot
+    {
+        IUnitOfWork UnitOfWork { get; }
+        TEntity Add(TEntity entity);
+        Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+        TEntity Update(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+        bool Remove(Entity entity);
+        Task<bool> RemoveAsync(Entity entity);
+    }
+}
