457cc35f15ce9c6c5193bf432f325e623dd1941c
chen <635903897@qq.com>
2025-07-24 17:42:20 +0800

    TODO：管道行为拆分为前后行为

--- Domain.Abstraction/Mediator/IPipelineBehavior.cs
+++ Domain.Abstraction/Mediator/IPipelineBehavior.cs
@@ -2,23 +2,30 @@
 namespace Domain.Abstraction.Mediator
 {
     /// <summary>
-    /// 管道行为
+    /// 前管道行为
     /// </summary>
-    /// <typeparam name="TRequest"></typeparam>
-    /// <typeparam name="TResponse"></typeparam>
-    public interface IPipelineBehavior<TRequest, TResponse>
-        where TRequest : IRequest<TResponse>
+    /// <typeparam name="TRequest">请求类型</typeparam>
+    /// <typeparam name="TResponse">响应类型</typeparam>
+    public interface IPipelineBehaviorBefore<TRequest, TResponse>
     {
         /// <summary>
-        /// 执行命令前的行为
+        /// 执行请求前的行为
         /// </summary>
         /// <param name="request">请求</param>
         /// <param name="cancellationToken">取消token</param>
         /// <returns></returns>
         Task<PipelineResponse<TResponse>?> Before(TRequest request, CancellationToken cancellationToken = default);
+    }
 
+    /// <summary>
+    /// 后管道行为
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface IPipelineBehaviorAfter<TRequest, TResponse>
+    {
         /// <summary>
-        /// 执行命令后的行为
+        /// 执行请求后的行为
         /// </summary>
         /// <param name="request">请求</param>
         /// <param name="cancellationToken">取消token</param>
@@ -27,9 +34,34 @@ namespace Domain.Abstraction.Mediator
     }
 
     /// <summary>
-    /// 
+    /// 管道行为
     /// </summary>
-    /// <typeparam name="TResponse"></typeparam>
+    /// <typeparam name="TRequest">请求类型</typeparam>
+    /// <typeparam name="TResponse">响应类型</typeparam>
+    public interface IPipelineBehavior<TRequest, TResponse> : IPipelineBehaviorBefore<TRequest, TResponse>, IPipelineBehaviorAfter<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        /// <summary>
+        /// 执行请求前的行为
+        /// </summary>
+        /// <param name="request">请求</param>
+        /// <param name="cancellationToken">取消token</param>
+        /// <returns></returns>
+        Task<PipelineResponse<TResponse>?> Before(TRequest request, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// 执行请求后的行为
+        /// </summary>
+        /// <param name="request">请求</param>
+        /// <param name="cancellationToken">取消token</param>
+        /// <returns></returns>
+        Task<PipelineResponse<TResponse>?> After(TRequest request, CancellationToken cancellationToken = default);
+    }
+
+    /// <summary>
+    /// 管道行为响应结果
+    /// </summary>
+    /// <typeparam name="TResponse">响应类型</typeparam>
     public sealed record PipelineResponse<TResponse>
     {
         /// <summary>
@@ -42,7 +74,7 @@ namespace Domain.Abstraction.Mediator
         /// </summary>
         public PipelineResponse()
         {
-            
+
         }
 
         /// <summary>
